08cb4691724b1e029b66b66603b9e01d7696cc20
njiddasalifu <njiddasalifu@gmail.com>
2023-03-04 23:38:35 +0100

    a simple server to listen to a request and responding in nodejs

---
+++ simpleServerFunction.js
@@ -0,0 +1,16 @@
+
+// this is a simple server listening to a request and responding
+// arrow function is also use
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('hello salif');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server rinning...');
+    // at this level check the console to see the log message
+    // open browser and go to 127.0.0.1:3000 to see results
+})
