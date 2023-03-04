d3bf10ae8aabd3ad5c4c07ff0b3c1a97fa7da0bd
njiddasalifu <njiddasalifu@gmail.com>
2023-03-04 23:52:41 +0100

    a simple server to listen to a request and responding in nodejs

--- simpleServerFunction.js
+++ simpleServerFunction.js
@@ -5,8 +5,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Welcome to my page');
+    res.setHeader('Content-type', 'text/html');
+    res.end('<h1>Welcome to my page!</h1>');
 });
 
 server.listen(3000, '127.0.0.1', () => {
