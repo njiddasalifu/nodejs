4486f98d637cfcfba97c800bac00f20b20de5e80
njiddasalifu <njiddasalifu@gmail.com>
2023-03-05 01:34:50 +0100

    POST request also added

--- customers/src/app.js
+++ customers/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+// this is making a GET request ti the server
 app.get('/', (req, res) => {
-    res.send('hello from EL-SALIF LDT');
+    res.send('Hello from EL-SALIF LDT\n This is a GET request');
 });
+// making POST request to the server
+app.post('/', (req, res)=> {
+    res.send("This is a POST request")
+})
  app.listen(PORT, () => {
     console.log('App is listening on port ' + PORT);
  });
